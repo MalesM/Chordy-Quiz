bb24ce339f95e93139de06c33f7588576f5c11c6
Gospoda_Males <markommales@gmail.com>
2018-01-24 23:46:24 +0100

    lista sa pustanjem zvukova

--- application/Screens/ChordList.js
+++ application/Screens/ChordList.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import { Text, FlatList } from 'react-native';
 import ChordListItem from '../Components/ChordListItem';
+import Sound from 'react-native-sound';
 
 import Chords from '../Config/Chords';
 
 export default class ChordList extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.samples = [];
+
+    for(var i = 0; i < Chords.length; i++){
+      this.samples.push(new Sound(Chords[i].sample, Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+          console.log('failed to load the sound', error);
+          return;
+        }
+        // loaded successfully
+        console.log('success');
+      }));
+    }
+
+    
+  }
+
 
   _keyExtractor = (item, index) => item.id;
   
 
   _playChord (idd) {
     console.log(idd);
+
+    //this.samples[idd].stop(() => {
+      this.samples[idd].play();
+    //});
+    
+    
   }
 
   render(){
