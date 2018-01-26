3061b1402448a1022f13fc612b37b35bfa83c0b7
Gospoda_Males <markommales@gmail.com>
2018-01-26 17:59:19 +0100

    pocetak pravljenja igre

--- application/Screens/GameScreen.js
+++ application/Screens/GameScreen.js
@@ -1,33 +1,74 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, ActivityIndicator } from 'react-native';
+import Chords from '../Config/Chords';
+import Sound from 'react-native-sound';
 
 export default class GameScreen extends React.Component {
+  constructor(props) {
+    super(props);
 
-  onBtnPress(){}
+    this.samples = [];
+    this.names = [];
+
+    for(let a = 0; a < 4; a++){
+      this.names.push(Chords[a]);
+    }
+
+    this.state = {
+      chordsGame: []
+    }
+
+  }
+
+  componentWillMount() {
+    for (var i = 0; i < 4; i++) {
+      this.samples.push(new Sound(Chords[i].sample, Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+          console.log('failed to load the sound', error);
+          return;
+        }
+        // loaded successfully
+
+        console.log('success');
+      }));
+    }
+    this.setState({
+      chordsGame: this.names
+    });
+
+  }
+
+  onBtnPress() { }
 
 
   render() {
+
+    console.log('Prvo ucitavanje');
     return (
+
       <View style={styles.buttonsContainer}>
-        <View style={styles.playButtonC}>
-          <Button title='PLAY'  onPress={() => this.onBtnPress()} />
-        </View>
-        <View style={styles.upBtns}>
-          <View style={styles.leftBtn}>
-            <Button title='1'  onPress={() => this.onBtnPress()}/>
-          </View>
-          <View style={styles.rightBtn}>
-            <Button title='2' onPress={() => this.onBtnPress()}/>
-          </View>
-        </View>
-        <View style={styles.downBtns}>
-          <View style={styles.leftBtn}>
-            <Button title='3' onPress={() => this.onBtnPress()}/>
-          </View>
-          <View style={styles.rightBtn}>
-          <Button title='4'onPress={() => this.onBtnPress()} />
-          </View>
-        </View>
+        {this.state.chordsGame.length === 0 ? <ActivityIndicator size="large" color="#0000ff" /> :
+          <View style={{ flex: 1 }}>
+            <View style={styles.playButtonC}>
+              <Button title='PLAY' onPress={() => this.onBtnPress()} />
+            </View>
+            <View style={styles.upBtns}>
+              <View style={styles.leftBtn}>
+                <Button title={this.state.chordsGame[0].name} onPress={() => this.onBtnPress()} />
+              </View>
+              <View style={styles.rightBtn}>
+                <Button title='2' onPress={() => this.onBtnPress()} />
+              </View>
+            </View>
+            <View style={styles.downBtns}>
+              <View style={styles.leftBtn}>
+                <Button title='3' onPress={() => this.onBtnPress()} />
+              </View>
+              <View style={styles.rightBtn}>
+                <Button title='4' onPress={() => this.onBtnPress()} />
+              </View>
+            </View>
+          </View>}
       </View>
     );
   }
